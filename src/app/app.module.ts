a4511512f30f43a782b1d998aba8044369a1f5aa
thierno mbodji <thiernosalif.mbodji@uvs.edu.sn>
2022-03-20 17:36:30 +0100

    content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackOfficeComponent } from './back-office/back-office.component';
 import { FrontOfficeComponent } from './front-office/front-office.component';
+import { SidebarComponent } from './back-office/sidebar/sidebar.component';
+import { NavbarComponent } from './back-office/navbar/navbar.component';
+import { FooterComponent } from './back-office/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BackOfficeComponent,
-    FrontOfficeComponent
+    FrontOfficeComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
